12d52fea73b806ed874a4b7a8ba1fd9da424668a
Almad <bugs@almad.net>
2010-08-08 22:32:31 +0200

    We're considering gh origin now

--- bin/merge.to.release.sh
+++ bin/merge.to.release.sh
@@ -2,7 +2,7 @@
 
 ./bin/run.command.py 'git checkout release'
 ./bin/run.command.py 'git merge master'
-./bin/run.command.py 'git push gh'
+./bin/run.command.py 'git push'
 ./bin/run.command.py 'git checkout master'
 ./bin/run.command.py 'git pull'
 
